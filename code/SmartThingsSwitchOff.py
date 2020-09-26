bd89a1fa34d3b3d1a02c3791c2db72ee4fa12dff
Helen Hanks <helenhanks@users.noreply.github.com>
2020-09-26 18:30:28 -0500

    Add files via upload

---
+++ code/SmartThingsSwitchOff.py
@@ -0,0 +1,12 @@
+import requests
+
+# Read in SmartThings token and endpoint from separate file
+import SmartThingsCredentials as credentials
+
+# Define headers
+headers = {
+    'Authorization': 'Bearer ' + credentials.token,
+}
+
+# Build on command
+response = requests.put(credentials.endpoint + '/switches/off', headers=headers)
