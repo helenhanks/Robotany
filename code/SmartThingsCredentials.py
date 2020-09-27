5ba90cbc487c7f24bc7dda551202a501c6556c97
Helen Hanks <helenhanks@users.noreply.github.com>
2020-09-26 20:08:17 -0500

    Update and rename SmartThingsCredentials to SmartThingsCredentials.py

---
+++ code/SmartThingsCredentials.py
@@ -0,0 +1,9 @@
+# Obtain these things from your app settings inside SmartThings
+# Note, your redirect URI can be literally any website
+ClientID = 'insert client ID here'
+ClientSecret = 'insert client secret here'
+RedirectURI = 'insert rediret uri here'
+
+# Once you have been through the OAuth process, define the two items below
+token = 'insert token string here'
+endpoint = 'insert endpoint string here'
